e7ba078bcddf839d0446d37c2dab9a830f31c820
GlebZyryanov <darrenshen02@gmail.com>
2023-08-01 14:34:50 +0300

    создал бд, порт и запустил сервер, создал функцию для подключения к базе данных

---
+++ index.js
@@ -0,0 +1,20 @@
+ require('dotenv').config()
+ const express = require('express')
+const sequelize = require('./db')
+
+ const PORT = process.env.PORT || 5000;
+
+ const app = express();
+
+//функция для подключения к базе данных
+const start = async()=>{
+    try{
+       await sequelize.authenticate()//эта функция устанавливает подключение к бд
+       await sequelize.sync() // эта функция сверяет состояние бд со схемой бд
+        app.listen(PORT, ()=>console.log(`server starts on port ${PORT}`))
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start()
