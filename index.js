e40869b138e79f200d0e5d8b4258b9f0c47d06ce
GlebZyryanov <darrenshen02@gmail.com>
2023-08-04 22:50:28 +0300

    подключил модели, отправился запрос на создание таблиц

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
- require('dotenv').config()
- const express = require('express')
-const sequelize = require('./db')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
 
- const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
- const app = express();
+const app = express();
 
 //функция для подключения к базе данных
-const start = async()=>{
-    try{
-       await sequelize.authenticate()//эта функция устанавливает подключение к бд
-       await sequelize.sync() // эта функция сверяет состояние бд со схемой бд
-        app.listen(PORT, ()=>console.log(`server starts on port ${PORT}`))
-    }catch(e){
-        console.log(e)
-    }
-}
+const start = async () => {
+  try {
+    await sequelize.authenticate(); //эта функция устанавливает подключение к бд
+    await sequelize.sync(); // эта функция сверяет состояние бд со схемой бд
+    app.listen(PORT, () => console.log(`server starts on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
